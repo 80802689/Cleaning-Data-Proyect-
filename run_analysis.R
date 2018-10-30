70730aeeb21814cda0226b9054da66c9f50e4f5a
cegavi70 <cegavi70@gmail.com>
2018-10-30 11:36:30 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,16 @@
+## Create one R script called run_analysis.R that does the following:
+## 1. Merges the training and the test sets to create one data set.
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+## 3. Uses descriptive activity names to name the activities in the data set
+## 4. Appropriately labels the data set with descriptive activity names.
+## 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+
+## configure the working directory, download the file to be read
+
 getwd()
 setwd("C:/Users/cesar ascencio/Desktop/Data_Science_especialización/Curso3 cleaning data/Proyecto")
-library(data.table)
+install.packages("data.table")
 library(data.table)
 url = 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
 if (!file.exists('./cesar Data1.zip')){
@@ -8,23 +18,28 @@ if (!file.exists('./cesar Data1.zip')){
   unzip("cesar Data1.zip", exdir = getwd())
 }
 
-####Merges the training and the test sets to create one data set
-## test data:
+
+########### 1 Merges the training and the test sets to create one data set.
+
+
+## # Load and process X_test & y_test data
+
 XTest<- read.table("UCI HAR Dataset/test/X_test.txt")
 YTest<- read.table("UCI HAR Dataset/test/Y_test.txt")
 SubjectTest <-read.table("UCI HAR Dataset/test/subject_test.txt")
 
+## train data
 
-## train data:
 XTrain<- read.table("UCI HAR Dataset/train/X_train.txt")
 YTrain<- read.table("UCI HAR Dataset/train/Y_train.txt")
 SubjectTrain <-read.table("UCI HAR Dataset/train/subject_train.txt")
 
 ## features and activity
+
 features<-read.table("UCI HAR Dataset/features.txt")
 activity<-read.table("UCI HAR Dataset/activity_labels.txt")
 
-##Part1 - merges train and test data in one dataset (full dataset at the end)
+      ##Part1 - merges train and test data in one dataset (full dataset at the end)
 
 X<-rbind(XTest, XTrain)
 Y<-rbind(YTest, YTrain)
@@ -35,34 +50,40 @@ dim(X); dim(Y); dim(Subject)
 
 index<-grep("mean\\(\\)|std\\(\\)", features[,2]) ##getting features indeces which contain mean() and std() in their name
 length(index) ## count of feature
+X<-X[,index]  ## getting only variables with mean/stdev
+dim(X)
 
-## Uses descriptive activity names to name the activities in the data set
+## 3 Uses descriptive activity names to name the activities in the data set
 
-Y[,1]<-activity[Y[,1],2] ## replacing numeric values with lookup value from activity.txt; won't reorder Y set
-head(Y)
+Y[,1]<-activity[Y[,1],2] ## ## replacing numeric values with lookup value from activity.txt; won't reorder Y set
 
-
-Appropriately labels the data set with descriptive variable names.
+### 4 Appropriately labels the data set with descriptive variable names.
 
 names<-features[index,2] ## getting names for variables
-    
+head(features)  
 names(X)<-names ## updating colNames for new dataset
 names(Subject)<-"SubjectID"
 names(Y)<-"Activity"
     
 CleanedData<-cbind(Subject, Y, X)
-head(CleanedData[,c(1:4)]) ## first 5
-
+head(CleanedData[,c(1:4)]) ## see first 5 files
+dim(CleanedData)
 
 
 ### 5  From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-CleanedData<-data.table(CleanedData)
-TidyData <- CleanedData[, lapply(.SD, mean), by = 'SubjectID,Activity'] ## features average by Subject and by activity
+CleanedData2<-data.table(CleanedData)
+names(CleanedData2)
+TidyData <- CleanedData2[, lapply(.SD, mean), by = 'SubjectID,Activity'] ## features average by Subject and by activity
 dim(TidyData)
-	
-write.table(TidyData, file = "Tidy.txt", row.names = FALSE)
-head(TidyData[order(SubjectID)][,c(1:4), with = FALSE],12)
+head(TidyData[,1:4])
+
+TidyData <- TidyData[order(SubjectID,Activity),]  ##Sort data by variable and by subject	
+write.table(TidyData, file = "Tidy.txt", row.names = FALSE)  ## Download the data in txt
+
+
+
+
 
 
 
